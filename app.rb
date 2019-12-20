a5b359de329170980bb03277b5d86e610895034f
jonathansalfen <jonathan.salfen@gmail.com>
2019-12-20 09:06:49 -0800

    finish tutorial

--- app.rb
+++ app.rb
@@ -1,23 +1,34 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/album')
+require('./lib/song')
 require('pry')
 also_reload('lib/**/*.rb')
 
 
-
-get('/test') do
-  @something = "this is a variable"
-  erb(:whatever)
-end
+# album routes
 
 get('/') do
   @albums = Album.all
   erb(:albums)
 end
 
+get('/test') do
+  @something = "this is a variable"
+  erb(:whatever)
+end
+
 get('/albums') do
   @albums = Album.all
+  erb(:albums) #added
+end
+
+post('/albums') do
+  name = params[:album_name]
+  album = Album.new(name, nil)
+  album.save()
+  @albums = Album.all()
+  erb(:albums)
 end
 
 get('/albums/new') do
@@ -26,18 +37,16 @@ end
 
 get('/albums/:id') do
   @album = Album.find(params[:id].to_i())
-  @album.update(params[:name])
-  @albums = Album.all
+  @album.update(params[:name]) #taken
+  @albums = Album.all #taken
   erb(:album)
 end
 
-get('/albums/:id/edit') do
-  @album = Album.find(params[:id].to_i())
-  erb(:edit_album)
-end
-
 patch('/albums/:id') do
-  "This route will update an album. We can't reach it with a URL. In a future lesson, we will use a form that specifies a PATCH action to reach this route."
+  @album = Album.find(params[:id].to_i()) #added
+  @album.update(params[:name]) #added
+  @albums = Album.all #added
+  erb(:albums) #added
 end
 
 delete('/albums/:id') do
@@ -47,14 +56,39 @@ delete('/albums/:id') do
   erb(:albums)
 end
 
-get('/custom_route') do
-  "We can even create custom routes, but we should only do this when needed."
+get('/albums/:id/edit') do
+  @album = Album.find(params[:id].to_i())
+  erb(:edit_album)
 end
 
-post('/albums') do
-  name = params[:album_name]
-  album = Album.new(name, nil)
-  album.save()
-  @albums = Album.all() # Adding this line will fix the error.
-  erb(:albums)
+# song routes
+
+# Get the detail for a specific song such as lyrics and songwriters.
+get('/albums/:id/songs/:song_id') do
+  @song = Song.find(params[:song_id].to_i())
+  erb(:song)
+end
+
+# Post a new song. After the song is added, Sinatra will route to the view for the album the song belongs to.
+post('/albums/:id/songs') do
+  @album = Album.find(params[:id].to_i())
+  song = Song.new(params[:song_name], @album.id, nil)
+  song.save()
+  erb(:album)
+end
+
+# Edit a song and then route back to the album view.
+patch('/albums/:id/songs/:song_id') do
+  @album = Album.find(params[:id].to_i())
+  song = Song.find(params[:song_id].to_i())
+  song.update(params[:name], @album.id)
+  erb(:album)
+end
+
+# Delete a song and then route back to the album view.
+delete('/albums/:id/songs/:song_id') do
+  song = Song.find(params[:song_id].to_i())
+  song.delete
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
 end
--- lib/album.rb
+++ lib/album.rb
@@ -1,12 +1,12 @@
 class Album
-  attr_reader :id, :name #Our new save method will need reader methods.
+  attr_reader :id, :name
 
   @@albums = {}
-  @@total_rows = 0 # We've added a class variable to keep track of total rows and increment the value when an ALbum is added.
+  @@total_rows = 0
 
-  def initialize(name, id) # We've added id as a second parameter.
+  def initialize(name, id)
     @name = name
-    @id = id || @@total_rows += 1  # We've added code to handle the id.
+    @id = id || @@total_rows += 1
   end
 
   def self.all
@@ -19,19 +19,24 @@ class Album
 
   def ==(album_to_compare)
     self.name() == album_to_compare.name()
+    # this is just to demonstrate a point.  One would not want to do this regularly unless they had a very good reason to do so
   end
 
   def self.clear
     @@albums = {}
     @@total_rows = 0
   end
+
   def self.find(id)
     @@albums[id]
   end
+
   def update(name)
     @name = name
   end
+
   def delete
     @@albums.delete(self.id)
   end
+
 end
